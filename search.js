6cc6fc4a337d400f554b31a9bfe9bdc2755cb23e
sergey.pavlov <force.space@rambler.ru>
2026-08-05 12:55:20 +0300

    Тема

--- search.js
+++ search.js
@@ -1,20 +1,59 @@
 const form = document.querySelector('.js-search-form');
 const select = form.querySelector('.js-search-engine-select');
+const selectWrapper = form.querySelector('.select-wrapper');
 const input = form.querySelector('.js-search-input');
 const button = form.querySelector('.search-button');
+const themeSelect = document.querySelector('.js-theme-select');
+const THEME_STORAGE_KEY = 'new-tab-theme';
+const DEFAULT_THEME = 'islands-dark';
+
+const THEMES = new Set([
+    'islands-dark',
+    'islands-light',
+    'islands-darcula',
+    'dark',
+    'light',
+    'light-header',
+    'darcula',
+    'high-contrast',
+]);
+
+const LEGACY_THEMES = {
+    dark: 'islands-dark',
+    light: 'islands-light',
+};
 
 const MODIFIERS = {
     yandex: {
         formAction: 'https://yandex.ru/search/',
         inputName: 'text',
+        wrapperClass: 'select-wrapper--yandex',
+        selectClass: 'search-engine-select--yandex',
         buttonClass: 'search-button--yandex',
     },
     google: {
         formAction: 'https://www.google.com/search',
         inputName: 'q',
+        wrapperClass: 'select-wrapper--google',
+        selectClass: 'search-engine-select--google',
         buttonClass: 'search-button--google',
     }
 };
+
+function getInitialTheme() {
+    const savedTheme = localStorage.getItem(THEME_STORAGE_KEY);
+    const theme = LEGACY_THEMES[savedTheme] || savedTheme;
+
+    return THEMES.has(theme) ? theme : DEFAULT_THEME;
+}
+
+function setTheme(theme) {
+    if (!THEMES.has(theme)) return;
+
+    document.body.dataset.theme = theme;
+    localStorage.setItem(THEME_STORAGE_KEY, theme);
+    themeSelect.value = theme;
+}
 
 function updateFormAttributes(engine) {
     if (!MODIFIERS[engine]) return;
@@ -24,11 +63,15 @@ function updateFormAttributes(engine) {
 }
 
 function updateStyles(engine) {
-    Object.values(MODIFIERS).forEach(({ buttonClass }) => {
+    Object.values(MODIFIERS).forEach(({ wrapperClass, selectClass, buttonClass }) => {
+        selectWrapper.classList.remove(wrapperClass);
+        select.classList.remove(selectClass);
         button.classList.remove(buttonClass);
     });
 
     if (MODIFIERS[engine]) {
+        selectWrapper.classList.add(MODIFIERS[engine].wrapperClass);
+        select.classList.add(MODIFIERS[engine].selectClass);
         button.classList.add(MODIFIERS[engine].buttonClass);
     }
 }
@@ -40,8 +83,13 @@ function onSelectChange() {
 }
 
 onSelectChange();
+setTheme(getInitialTheme());
 
 select.addEventListener('change', onSelectChange);
+
+themeSelect.addEventListener('change', () => {
+    setTheme(themeSelect.value);
+});
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
